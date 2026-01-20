f99276d3829aead5923ba4ed45a4107ca4cc9446
Santhosh S <santhosh.s@quadrasystems.net>
2026-01-20 09:44:08 +0530

    Modified in Approvals

--- src/webparts/vGuardDms/Pages/L1Approver/L1ApproverDashboard.tsx
+++ src/webparts/vGuardDms/Pages/L1Approver/L1ApproverDashboard.tsx
@@ -1,6 +1,25 @@
 import * as React from 'react';
+import { getCurrentUser } from '../../../../Service/commonService';
+import { fetchApproverData } from '../../components/Helper/helperService';
 
 const L1Approver = () => {
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const fetchItems = async () => {
+    try {
+      const currentUser = await getCurrentUser();
+      const response = await fetchApproverData(currentUser.mail || currentUser.userPrincipalName);
+      console.log('response data is', response);
+      
+    } catch(error) {
+      console.log("Error While fetching the data");
+    }
+  }
+
   return (
     <div>
       L1Approver
